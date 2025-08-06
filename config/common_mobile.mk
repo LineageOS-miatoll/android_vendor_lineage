a133ebd5dfba2f504dfa79244b9c38fc1984ef82
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2025-08-06 14:50:48 +0000

    vendor: Use Pixel charger animation
    Squashed with the following:
    
    Author: Arseniy Graur <argraur@gmail.com>
    Date:   Fri Jan 24 14:08:14 2020 +0000
    
        vendor: Use Pixel charger animation
    
        Change-Id: I89ac5aa6c4dec0ded4524e94dc07fd268690b5f4
    
    Author: chrisw444 <wandersonrodriguesf1@gmail.com>
    Date:   Sat Jan 22 19:21:46 2022 +0000
    
        vendor: charger: Update Pixel charger animation from SQ1D.220105.007
    
        Change-Id: I75b1fcd9580a7961cb64620c828a58e5977f2840
    
    Author: Tim Zimmermann <tim@linux4.de>
    Date:   Thu Sep 14 16:38:18 2023 +0530
    
        vendor: Support charger on vendor
    
        Change-Id: Id402ddeae8e43392d0a9f505e5cf4c162231d99f
    
    Co-authored-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Co-authored-by: Tim Zimmermann <tim@linux4.de>

--- config/common_mobile.mk
+++ config/common_mobile.mk
@@ -39,13 +39,9 @@ PRODUCT_PACKAGES += \
 
 # Charger
 PRODUCT_PACKAGES += \
-    charger_res_images
-
-ifneq ($(WITH_LINEAGE_CHARGER),false)
-PRODUCT_PACKAGES += \
-    lineage_charger_animation \
-    lineage_charger_animation_vendor
-endif
+    charger_res_images \
+    product_charger_res_images \
+    product_charger_res_images_vendor
 
 # Customizations
 PRODUCT_PACKAGES += \
